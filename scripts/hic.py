8250a6ecea8005405bb3078c8d7a306fc763209d
Keivan Amini <keivan.amini@outlook.it>
2023-08-25 20:15:28 +0200

    divided classes into different scripts. added docstrings for the HiC class. added get_block_df function

--- scripts/hic.py
+++ scripts/hic.py
@@ -1,186 +1,212 @@
+"""
+This script has the aim to construct the HiC class, useful to work
+on an adjacency matrix and thereafter perform a statistical analysis.
+It requires also a metadata file stored in csv or .xlsx containing in
+the columns the name of the involved chromosomes, the start and the end 
+of the nodes index in the newtork associated with the chromosomes.
+"""
 import networkx as nx
 import pandas as pd
-import matplotlib.pyplot as plt
-from node2vec import Node2Vec
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-from matplotlib.colors import ListedColormap
-
+import numpy as np
 #openpyxl needed
+
 
 class HiC:
 
-    def __init__(self, path_csv, path_metadata):
-        self.path = path_csv
-        self.path_metadata = path_metadata
-        self.name = self.path.replace('..\data\\', '')
-        self.name = self.name.replace('.csv', '') #to optimize: do not like the second repetition fo the function
+    """
+    Description
+    -----------
+    This class creates and prepares dataframes structures,
+    starting from an Hi-C .csv data interpreted as an adjacency
+    matrix and a metadata file, containing informations about
+    chromosomes.
 
+    Methods
+    -------
+        get_dfs()
+        remove_empty_axis()
+        get_index_chromosomes()
+        get_nodes()
+        get_block_df()
+        get_graph()
+        get_block_graph()
 
-        self.get_df()
-        #can add a function to normalize the graph, if it is not already normalized.
-        self.get_metadata_df()
-        self.remove_empty_axis()
+    Attributes
+    ----------
+        name
+        matrix_df
+        metadata_df
+        index_chromosomes
+        attributes
+
+    """
+
+    def __init__(self, matrix_path: str,
+                 metadata_path: str) -> None:
+
+        """
+        Remove empty axis and extract diagonal blocks, making
+        the dataframe ready to undergo different algorithms.
+
+        Parameters
+        ----------
+            matrix_path (str): 
+                Path containing the adjacency matrix in .csv extension.
+            metadata_path (str): 
+                Path containing the metadata in .xlsx or .csv extensions.
+        """
+
+        self.name = matrix_path.replace(r'..\\data\\', '').replace('.csv', '')
+        self.matrix_df, self.metadata_df = None, None
+
+        self.get_dfs(matrix_path, metadata_path)
+        self.get_index_chromosomes()
+
         self.get_graph()
+        self.get_block_graph()
 
-    def get_df(self):
-        self.df = pd.read_csv(self.path, header = None)
-
-    def get_metadata_df(self): #slow. to improve!
-        self.metadata_df = pd.DataFrame(pd.read_excel(self.path_metadata))
-
-    def remove_empty_axis(self):
-        self.df = self.df.loc[:, (self.df != 0).any(axis=0)]
-        self.df = self.df.loc[(self.df!=0).any(axis=1)]
-
-    def get_graph(self):
-        self.G = nx.from_pandas_adjacency(self.df)
-        list = []
-        for i in range(len(self.metadata_df.chr)):
-            list += ([self.metadata_df.chr[i]] * (self.metadata_df.end[i] - self.metadata_df.start[i]+1))
-        dict = {index: value for index, value in enumerate(list)}
-        nx.set_node_attributes(self.G, dict, 'chromosome')
-        self.attributes = nx.get_node_attributes(self.G, 'chromosome')
+    def get_dfs(self, matrix_path: str,
+                metadata_path: str) -> None:
+        """
+        Create two dataframes: one containing the adjacency 
+        matrix and one containing the metadata. At the end,
+        remove empty axis from the matrix_df attribute.
         
-    def draw_network(self):
-        nx.draw(self.G, arrows = True)
-        plt.show()
+        Parameters
+        ----------
+            matrix_path (str):
+                string containing the path of the adjacency matrix.
+            metadata_path (str): 
+                string containing the path of the metadata file.
+        """
+        self.matrix_df = pd.read_csv(matrix_path, header = None)
+        if metadata_path.endswith(('xlsx',  'xls')):
+            self.metadata_df = pd.DataFrame(pd.read_excel(metadata_path))
+        elif metadata_path.endswith('csv'):
+            self.metadata_df = pd.read_csv(metadata_path)
+        else:
+            raise ValueError(
+                "Unsupported file format. Only Excel (xlsx, xls) and csv files are supported.")
+        self.remove_empty_axis()
 
+    def remove_empty_axis(self) -> None:
+        """
+        Remove empty axis from the dataframe containing the
+        adjacency matrix coming from the Hi-C data.
+        """
+        self.matrix_df = self.matrix_df.loc[:, (self.matrix_df != 0).any(axis = 0)]
+        self.matrix_df = self.matrix_df.loc[(self.matrix_df != 0).any(axis = 1)]
 
+    def get_index_chromosomes(self) -> None:
+        """
+        Create a list containing the index referred to the
+        starting node of each chromosome in the network.
+        """
+        self.index_chromosomes = self.metadata_df.end.values.tolist()
+        self.index_chromosomes = np.append(self.index_chromosomes, 0, 0)
 
-class Algorithms:
+    def get_nodes(self, chromosome: int) -> np.ndarray:
+        """
+        Given a chromosome, return a np.ndarray containing
+        the index nodes associated with the chromosome.
 
-    def __init__(self, hi_c):
+        Parameter
+        ---------
+            chromosome (int):
+                integer value representing the chromosome
+                we want to get the index nodes.
 
-        self.hi_c = hi_c
-        self.G = hi_c.G
-        self.name = hi_c.name
-        self.attributes = hi_c.attributes
+        Return
+        -------
+            nodes (np.ndarray):
+                index nodes associated with the input
+                chromosome.
 
+        """
+        nodes = np.arange(self.index_chromosomes[chromosome],
+                          self.index_chromosomes[chromosome + 1])
+        return nodes
 
-        # Node2Vec parameters
-        self.D = 10
-        self.P = 1
-        self.Q = 0.5
-        self.WL = 300
-        self.embedding_path = '..\model\-' + str(self.name) + '_embedding.csv'
-        self.df = None
+    def get_block_df(self,
+                     selected_chromosome: int = 1) -> list:
+        """
+        Create a list containing data frames corresponding to the
+        selected chromosome and all the other chromosomes.
 
-        # PCA parameters
-        self.old_dimensions = [dimension for dimension in range(self.D)]
-        self.number_components = 2
-        self.new_dimensions = [dimension for dimension in range(self.number_components)]
+        Parameter
+        ---------
+            selected_chromosome (int), optional:
+                Integer number between 0 and len(self.metadata_df), representing
+                the selected chromosome to which we are going to create different
+                block data frames, regarding all the possible couples between the
+                selected chromosome and all the other chromosomes.
+                Default is 1 (chr6), the one with unobservable traslocation.
+        Return
+        ------
+            block_dfs (list):
+                List containing the data frames considering only the
+                selected chromosome and all the other chromosomes.
+                From each df in block_dfs, the empty axes are removed.
+                
+        """
+        nodes = np.arange(self.index_chromosomes[-1])
+        chromosomes = np.arange(len(self.metadata_df.columns))
+        block_dfs = []
 
+        for chromosome in chromosomes:
+            selected_chromosome_nodes = self.get_nodes(selected_chromosome)
+            if chromosome != selected_chromosome:
+                nodes_to_keep = self.get_nodes(chromosome)
+                nodes_to_keep = np.append(nodes_to_keep, selected_chromosome_nodes)
+                nodes_to_drop = get_complementary_list(nodes_to_keep, nodes)
+                block_df = self.matrix_df.drop(index = nodes_to_drop,
+                                               columns = nodes_to_drop)
+                block_dfs.append(block_df)
+        return block_dfs
 
-    def node2vec(self):
-       self.node2vec = Node2Vec(self.G, dimensions = self.D, walk_length = self.WL,
-                                num_walks = 10, weight_key='weight', workers = 1,
-                                p = self.P, q = self.Q)
-       self.model = self.node2vec.fit(window = 10, min_count = 1, batch_words = 4)
-       self.model.wv.save_word2vec_format(self.embedding_path)
-       self.get_df()
-       self.normalize_features()
+    def get_graph(self): #TODO later
+        """
+        Get the graph structure of the Hi-C matrix using networkX
+        library, starting from the matrix_df attribute.
+        """
+        graph = nx.from_pandas_adjacency(self.matrix_df)
+        list_chromosomes = []
+        for index in range(len(self.metadata_df.chr)):
+            list_chromosomes += ([self.metadata_df.chr[index]] *
+                     (self.metadata_df.end[index] - self.metadata_df.start[index] + 1))
+        dict_chromosomes = {index: value for index, value in enumerate(list_chromosomes)}
+        nx.set_node_attributes(graph, dict_chromosomes, 'chromosome')
+        self.attributes = nx.get_node_attributes(graph, 'chromosome')
 
-    def get_df(self):
-        self.df = pd.read_csv(self.embedding_path, header = 0,
-                              names = self.old_dimensions, sep= ' ')
-        self.df.sort_index(inplace = True)
-        self.index_series = self.df.index.to_series()
+    def get_block_graph(self):
+        """
+        Get a list containing the graphs associated with the
+        block data frames, for each couple of chromosomes in
+        the metadata file.
+        """
+        block_dfs = self.get_block_df()
+        graphs_list = []
 
-    def normalize_features(self):
-        self.x = self.df.loc[:, self.old_dimensions].values
-        self.x = StandardScaler().fit_transform(self.x)
+        for block_df in block_dfs:
+            block_graph = nx.from_pandas_adjacency(block_df)
+            graphs_list.append(block_graph) # i think we do not need block attribute!
 
-    def pca(self):
-        self.pca = PCA(n_components = self.number_components)
-        principalComponents = self.pca.fit_transform(self.x)
-        self.principal_df = pd.DataFrame(data = principalComponents,
-                                         columns = self.new_dimensions,
-                                         index = self.index_series)
-        self.new_x = self.principal_df.loc[:,self.new_dimensions].values
+def get_complementary_list(main_list: list,
+                           reference_list: list) -> list:
+    """
+    Get a list of elements from the reference list that are not present in the main list.
 
-    # Graphics
+    Parameters
+    ----------
+        main_list: list
+            List of elements to be used as the base of comparison.
+        reference_list: list
+            List of elements from which complementary elements are extracted.
 
-    def get_plot_chromosome(self, type):
-
-        fig, ax = plt.subplots() 
-        ax.set_axisbelow(True)
-        ax.grid()
-
-        self.attributes = dict(sorted(self.attributes.items())) #order by number items
-        chromosomes = list(set((self.attributes.values())))
-        color_list = ['tab:blue','tab:green', 'tab:red', 'tab:orange', 'tab:pink']
-        color_dict = {chr: color for chr, color in zip(chromosomes, color_list)}
-        color_dict = dict(sorted(color_dict.items()))
-
-        for index, x in self.principal_df.iterrows():
-            ax.scatter(x[0], x[1], c = color_dict[self.attributes[index]], s = 7, label = str(self.attributes[index]))
-
-        ax.set_xlabel("First principal component")
-        ax.set_ylabel("Second principal component")
-        ax.legend()
-        ax.set_title(str(type))
-        legend_without_duplicate_labels(ax)
-
-        fig.savefig('..\plot\pca_chromosome_' + str(type) + '.png') #cambiare nome
-        plt.show()
-
-
-
-    def get_plot_chromosome_comparison(self, df1, df2): #non voglio self. voglio che self.attributes sia variabile globale
-
-        fig, ax = plt.subplots() 
-        ax.set_axisbelow(True)
-        ax.grid()
-
-
-        color_list = ['tab:blue','tab:green', 'tab:red', 'tab:orange', 'tab:pink']
-
-        self.attributes = dict(sorted(self.attributes.items()))
-        chromosomes = list(set((self.attributes.values())))
-        color_dict = {chr: color for chr, color in zip(chromosomes, color_list)}
-        color_dict = dict(sorted(color_dict.items()))
-
-        for index, x in df1.iterrows():
-            ax.scatter(x[0], x[1], c = color_dict[self.attributes[index]], marker = 'x', s = 30, label = str(self.attributes[index]))  #tumor
-
-        for index, x in df2.iterrows():
-            ax.scatter(x[0], x[1], c = color_dict[self.attributes[index]], marker = 'v', s = 30, label = str(self.attributes[index])) #healthy
-
-        ax.set_xlabel("First principal component")
-        ax.set_ylabel("Second principal component")
-        #aggiungere un ulteriore legenda con la distinzione tra tumore e cellula sana
-        ax.set_title('Chromosome comparison for healthy and tumor cell with PCA and Node2Vec')
-        legend_without_duplicate_labels(ax)
-        fig.savefig('..\plot\pca_chromosome_comparison.png') #cambiare nome
-        plt.show()
-
-
-def get_plot(features1, features2): #non mi piace il fatto che features1 debba essere per forza cancro
-    fig, ax = plt.subplots() 
-    ax.set_axisbelow(True)
-    ax.grid()
-    x1, y1 = features1[:,0], features1[:,1]
-    x2, y2 = features2[:,0], features2[:,1]
-    ax.scatter(x1, y1, color = "tab:red", s= 4, label = 'Cancer Hi-C') # da ottimizzare
-    ax.scatter(x2, y2, color = "tab:green", s = 4, label = 'Healthy Hi-C')
-    ax.set_title('PCA on node2vec algorithm')
-    ax.set_xlabel("First principal component")
-    ax.set_ylabel("Second principal component")
-    ax.legend()
-    fig.savefig('..\plot\pca.png') #cambiare nome
-    plt.show()
-
-
-
-
-
-    
-#avere il grafico a destra e avere una sola legenda
-
-
-def legend_without_duplicate_labels(ax):
-    handles, labels = ax.get_legend_handles_labels()
-    unique = [(h, l) for i, (h, l) in enumerate(zip(handles, labels)) if l not in labels[:i]]
-    ax.legend(*zip(*unique))
-
+    Return
+    ------
+        complementary_list: list
+            list containing elements from the reference_list that are not present in the main_list.
+    """
+    complementary_list = [num for num in reference_list if num not in main_list]
+    return complementary_list
