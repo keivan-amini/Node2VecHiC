999c680d3a38f58694db65fa0bed9e4186f29581
Keivan Amini <keivan.amini@outlook.it>
2023-09-20 19:32:13 +0200

    updated main

--- Node2VecHiC/main.py
+++ Node2VecHiC/main.py
@@ -1,5 +1,6 @@
 """
 Main script controlling the execution of the different modules.
+To run experiment, it is suggested to look at the tutorial instead of this file
 """
 import numpy as np
 
@@ -15,22 +16,19 @@ CANCER_PATH = '..\\data\\cancer_hic.csv'
 HEALTHY_PATH = '..\\data\\healthy_hic.csv'
 
 EMBEDDINGS_PATH = '..\\model\\blocks\\cancer_20-1.csv'
-EMBEDDINGS_PATH1 = '..\\model\\blocks\\cancer_20-6.csv' #IN REALTà 6-10 MA FACCIAMO UNA PROVA E VEDIAMO SE IL MODELLO è UGUALE!!!!
-EMBEDDINGS_PATH2 = '..\\model\\blocks\\cancer_20-X.csv' # da cambiare. attento ai nomi
+EMBEDDINGS_PATH1 = '..\\model\\blocks\\cancer_20-6.csv'
+EMBEDDINGS_PATH2 = '..\\model\\blocks\\cancer_20-X.csv'
 EMBEDDINGS_PATH3 = '..\\model\\blocks\\cancer_20-10.csv'
 
 
 metadata = Metadata(METADATA_PATH)
 cancer_hic = HiC(metadata, CANCER_PATH)
 
-#for df in block_dfs:
-#    print(df)
-
 
 # Node2Vec parameters
-N_DIMENSIONS = 10 # 10 #attenzione, parametro da cambiare ogni volta che non si genera node2vec ma che si prende direttamente il df dal folder model!!
-WALK_LENGTH = 300 #300
-NUM_WALKS = 10 #10
+N_DIMENSIONS = 10 
+WALK_LENGTH = 300 
+NUM_WALKS = 10
 WEIGHT_KEY = 'weight'
 WORKERS = 1
 P = 1
@@ -49,8 +47,8 @@ embeddings, indexes = run_node2vec(cancer_graph,
 
 #Blocks
 
-
-graph_block_lists = cancer_hic.get_block_graph()
+selected_chromosome = 1
+graph_block_lists = cancer_hic.get_block_graph(selected_chromosome)
 embeddings_path = [EMBEDDINGS_PATH, EMBEDDINGS_PATH1, EMBEDDINGS_PATH2, EMBEDDINGS_PATH3]
 embeddings = []
 
@@ -72,7 +70,6 @@ embeddings1, indexes1 = get_embeddings(N_DIMENSIONS, EMBEDDINGS_PATH1)
 embeddings2, indexes2 = get_embeddings(N_DIMENSIONS, EMBEDDINGS_PATH2)
 embeddings3, indexes3 = get_embeddings(N_DIMENSIONS, EMBEDDINGS_PATH3)
 
- # forse non è necessario far tornare indexes: da vedere se lo è in grafica!!
 """
 
 
@@ -80,23 +77,19 @@ embeddings3, indexes3 = get_embeddings(N_DIMENSIONS, EMBEDDINGS_PATH3)
 N_COMPONENTS = 2
 
 block = []
-selected_chromosome = cancer_hic.selected_chromosome
 for model in embeddings:
     principal_df = run_pca(data_frame=model,
                            n_components=N_COMPONENTS)
     block.append(principal_df)
 
 
-
-
-
 # Graphics
 
 cancer_graphics = Graphics(cancer_hic)
-#cancer_graphics.get_plot_chromosome(principal_df) #non mi fa impazzire. components dovrebbe essere dento cancer_graphics.
-cancer_graphics.get_plot_blocks(block, selected_chromosome)
+#cancer_graphics.get_plot_chromosome(principal_df)
+cancer_graphics.get_plot_blocks(block, selected_chromosome, False)
 
-# Blocks (notebooks?)
+# Blocks
 
 """
 graphs_list = cancer_hic.get_block_graph()
@@ -115,8 +108,3 @@ for graph in graphs_list:
     
 cancer_graphics.get_plot_blocks(principal_blocks_df)
 """
-    
-
-
-    
-
